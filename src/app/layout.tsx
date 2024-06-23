f177cbc3c6e7f7f0c868da7fe9d4a4f387f9588f
nnxst0 <netcrazy2546@gmail.com>
2024-06-23 10:51:41 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "John Farmer",
+  title: "Ant Tatungtuang",
   description: "Example personal website",
 };
 
